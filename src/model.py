94e24402404d003918f39353438733dfec65c956
doemeschaich <dominik.schaich@icloud.com>
2026-05-12 12:46:52 +0200

    feat: add MobileNetV3 transfer learning

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@
 Model definitions for plant disease classification.
 """
 import torch.nn as nn
+import torchvision.models as models
 
 
 class SimpleCNN(nn.Module):
@@ -41,3 +42,18 @@ class SimpleCNN(nn.Module):
         x = self.features(x)
         x = self.classifier(x)
         return x
+
+def create_mobilenet(num_classes: int = 38, freeze_backbone: bool = False):
+    """Create MobileNetV3-Small pretrained on ImageNet with a new classifier head."""
+    weights = models.MobileNet_V3_Small_Weights.IMAGENET1K_V1
+    model = models.mobilenet_v3_small(weights=weights)
+
+    if freeze_backbone:
+        for param in model.features.parameters():
+            param.requires_grad = False
+
+    # Replace the final classifier layer (1000 ImageNet classes -> our 38)
+    in_features = model.classifier[-1].in_features
+    model.classifier[-1] = nn.Linear(in_features, num_classes)
+
+    return model
